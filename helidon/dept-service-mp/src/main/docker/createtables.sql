fd3b2b1b3087ca4a23f1558ddd2396f52fb7f6f5
alexey_dolganov <alexey.dolganov@oracle.com>
2019-04-11 17:54:07 +0200

    add to createtables.sql

--- helidon/dept-service-mp/src/main/docker/createtables.sql
+++ helidon/dept-service-mp/src/main/docker/createtables.sql
@@ -14,3 +14,14 @@ CREATE TABLE [TrainingHistory] (
 [TrainingDate] DATE,
 [TrainingCost] INTEGER
 ); 
+
+CREATE TABLE departments ( 
+department_id NUMBER(4)
+, department_name VARCHAR2(30)
+CONSTRAINT dept_name_nn NOT NULL
+, manager_id NUMBER(6)
+, location_id NUMBER(4)
+);
+
+CREATE UNIQUE INDEX dept_id_pk
+         ON departments (department_id) ;
